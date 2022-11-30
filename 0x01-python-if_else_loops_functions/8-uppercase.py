329ceba8a5a51f9ded183a0a16a2f280735bc951
Bimbonium <bimbonium2@gmail.com>
2022-11-30 01:47:12 +0000

    Solve task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    case_definer = 0
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
             case_definer = 32
         else:
             case_definer = 0
         print('{}'.format(chr(ord(i) - case_definer)), end='')
-        print('')
+    print('')
