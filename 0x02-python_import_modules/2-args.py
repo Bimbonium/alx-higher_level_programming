491e6a9b544cfb3befd7ffd108f97354cd57fa67
Bimbonium <bimbonium2@gmail.com>
2022-12-01 23:40:45 +0000

    Solve the first 3 tasks

---
+++ 0x02-python_import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv as argvector
+arg_len = len(argvector)
+counter = 1  # to count from 1 to arg_len
+if arg_len == 0:
+    print('{} arguments.'.format(len(argvector)))
+elif arg_len == 1:
+    print('{} argument:'.format(len(argvector)))
+    print('{}: {}'.format(arg_len, argvector[arg_len - 1]))
+else:
+    print('{} arguments:'.format(len(argvector)))
+    for i in argvector:
+        while counter != arg_len + 1:
+            print('{}: {}'.format(counter, argvector[counter - 1]))
+            counter += 1
