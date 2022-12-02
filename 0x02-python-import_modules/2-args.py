9124adeeb6e6a1c469455d09b72fbfd0ae611977
Bimbonium <bimbonium2@gmail.com>
2022-12-02 00:01:53 +0000

    Solve task 3 again

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,14 +4,14 @@ if __name__ == "__main__":
     from sys import argv as argvector
     arg_len = len(argvector)
     counter = 1  # to count from 1 to arg_len
-    if arg_len == 0:
-        print('{} arguments.'.format(len(argvector)))
-    elif arg_len == 1:
-        print('{} argument:'.format(len(argvector)))
-        print('{}: {}'.format(arg_len, argvector[arg_len - 1]))
+    if arg_len == 1:
+        print('{} arguments.'.format(arg_len - 1))
+    elif arg_len == 2:
+        print('{} argument:'.format(arg_len - 1))
+        print('{}: {}'.format(arg_len - 1, argvector[arg_len - 1]))
     else:
-        print('{} arguments:'.format(len(argvector)))
+        print('{} arguments:'.format(arg_len - 1))
         for i in argvector:
-            while counter != arg_len + 1:
-                print('{}: {}'.format(counter, argvector[counter - 1]))
+            while counter != arg_len:
+                print('{}: {}'.format(counter, argvector[counter]))
                 counter += 1
