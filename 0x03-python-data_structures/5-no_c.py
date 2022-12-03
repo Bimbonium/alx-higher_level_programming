8302543cfd974bdc61f10b36beece671afe72507
Bimbonium <bimbonium2@gmail.com>
2022-12-03 22:42:47 +0000

    Solve task 2, 3, and 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 def no_c(my_string):
     new_str = ''
     for i in my_string:
-        if i not in 'Cc'
+        if i not in 'Cc':
         new_str += i
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for index, j in enumerate(i):
-            print('{:d}'.format(j), end ='')
+            print('{:d}'.format(j), end='')
             if index < len(i) - 1:
                 print('{}'.format(' '), end='')
         print()
