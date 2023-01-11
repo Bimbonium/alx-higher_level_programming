6add4150abb755a916c78b5c2c230322b556a903
Bimbonium <bimbonium2@gmail.com>
2023-01-11 21:46:10 +0000

    Solve task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Class to define a square, with an attribute size"""
 
+
 class Square:
     """Class with a size"""
-    def __init__(self,sizevar):
+    def __init__(self, sizevar):
         self.__size = sizevar
-
